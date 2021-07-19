65f3cd97a4cf2b4e5d0318ae9dac909b18715998
seanparnold <sean.p.arnold@gmail.com>
2021-07-19 13:28:18 -0700

    added languages feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,19 @@
+var languageButtonsEl = document.querySelector('#language-buttons');
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub Info Not Found');
+    }
+  });
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -24,12 +40,23 @@ var formSubmitHandler = function(event) {
     var username = nameInputEl.value.trim();
 
     if (username) {
-    getUserRepos(username);
-    nameInputEl.value = "";
+      getUserRepos(username);
+      nameInputEl.value = "";
     } else {
-    alert("Please enter a GitHub username");
+      alert("Please enter a GitHub username");
     }
   };
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute('data-language');
+  console.log(event);
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
 
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
@@ -83,3 +110,4 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
   
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
